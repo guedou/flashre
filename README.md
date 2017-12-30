114c6e8fc83d7574310469e7b23242f69b83b208
Guillaume Valadon <guillaume@valadon.net>
2017-12-30 15:24:03 +0100

    Refactoring and addition of the CameCase strategy

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 ## Naming strategies
 
+In practice, the "error" strategy find most of the functions. The "CamelCase"
+one only finds few more functions, and some false positives.
+
 ```
 python -m flashre.main naming ~/Projects/flashre/dumps/w-03/dump_w03-hw.bin --offset 0xC00000
+```
+
+```
+python -m flashre.main naming ~/Projects/flashre/dumps/w-03/dump_w03-hw.bin --offset 0xC00000 --strategy camelcase
 ```
--- flashre/naming.py
+++ flashre/naming.py
@@ -5,9 +5,10 @@ Functions naming strategies
 """
 
 import json
+import re
 
 from flashre.binaries_helpers import ReverseFlashairBinary
-from flashre.utils import r2_search_memory
+from flashre.utils import r2_search_memory, is_camelcase_str
 
 
 def print_r2_definitions(candidates):
@@ -19,37 +20,36 @@ def print_r2_definitions(candidates):
         addresses = candidates[fname]
         if len(addresses) > 1:
             for i in xrange(0, len(addresses)):
-                print "af %s_%d %s" % (fname, i, addresses.pop())
-        else:
-            print "af %s 0x%x" % (fname, addresses.pop() + 0xC00000)
+                print "af %s_%d 0x%x" % (fname, i, addresses.pop())
+        elif len(addresses) == 1:
+            print "af %s 0x%x" % (fname, addresses.pop())
 
 
-class NamingError(object):
+class Naming(object):
     """
-    Naming strategy based on error strings.
+    Generic naming methods used to easily experiment with new strategies
     """
 
     @classmethod
     def drop_string(cls, string):
-        """
-        Discard useless strings.
-        """
-        return not string.strip().endswith("] (error) %s:%d")
+        raise NotImplementedError("Naming.drop_string()")
+
+    @classmethod
+    def assemble_instructions(cls, rfb, address):
+        raise NotImplementedError("Naming.assemble_instructions()")
+
+    @classmethod
+    def is_valid_pattern(cls, rfb, prefix, offset):
+        raise NotImplementedError("Naming.is_valid_pattern()")
 
     @classmethod
     def lookup(cls, rfb, address):
         """
-        Look for MOV instructions that are using the string address.
-
-        Based on observations, they are "MOVU R1, <ADDRESS>" with a 24 bytes
-        immediate aka Major Opcode #13 MOVU.
+        Look for instructions that are using the string address.
         """
 
-        # Assemble a MOV that uses this address
-        mode = rfb.machine.dis_engine().attrib
-        instr = rfb.mn.fromstring("MOVU R1, 0x%x" % (address + rfb.offset), mode)
-        # Discard canditates that does not encode R1 on 3 bits
-        targets = [(x.encode("hex"), str(rfb.mn.dis(x, mode))) for x in rfb.mn.asm(instr, mode) if x[1] == '\xd1']
+        # Asssemble instructions
+        targets = cls.assemble_instructions(rfb, address)
 
         # Look for the offset of these instructions
         ret = []
@@ -59,31 +59,44 @@ class NamingError(object):
         return ret
 
     @classmethod
+    def assemble_mov13(cls, rfb, reg, address, filter_pattern):
+        """
+        Assemble MOV#13 instructions.
+        """
+
+        # Assemble a MOV that uses this address
+        mode = rfb.machine.dis_engine().attrib
+        instr = rfb.mn.fromstring("MOVU %s, 0x%x" % (reg, address + rfb.offset), mode)
+        # Discard candidates that does not encode the register on 3 bits
+        candidates = rfb.mn.asm(instr, mode)
+        targets = [(x.encode("hex"), str(rfb.mn.dis(x, mode))) for x in candidates
+                   if x[1] == filter_pattern]
+
+        return targets
+
+    @classmethod
+    def extract_prefix(cls, string):
+        """
+        Extract the function category from the format string.
+
+        Dummy implementation.
+        """
+        return string
+
+    @classmethod
     def check(cls, rfb, string, candidates):
         """
-        Extract possible candidates based on the "error" patterns:
-
-        Example:
-            0x00c11796      43d1c5c9       MOVU R1, 0xC9C543           ; r1=0xc9c543 "[APL] (error) %s:%d "
-            0x00c1179a      34d2c5c9       MOVU R2, 0xC9C534           ; r2=0xc9c534 "APPL_endBuffer"
-            0x00c1179e      01c3f901       MOV R3, 505                 ; r3=0x1f9 -> 0xffffff00
+        Extract possible candidates based on the current pattern.
         """
 
-        # Extract the function category from the format string
-        prefix = string[string.index("[")+1:string.index("]")].replace(" ", "")
-
         faddresses = set()
+        real_fname = ""
         for offset in candidates:
-            # Disassemble three instructions and look for the expected sequence
-            instructions = json.loads(rfb.r2p.cmd("pdj 3 @ 0x%x" % offset))
 
-            if not instructions[1]["opcode"].startswith("MOVU R2"):
+            # Validate the current pattern
+            real_fname = cls.is_valid_pattern(rfb, string, offset)
+            if len(real_fname) == 0:
                 continue
-
-            # Extract the immediate from the second instruction
-            fname_str_addr = instructions[1]["opcode"].split(",")[1][0:]
-            fname_str_addr = int(fname_str_addr, 16)
-            real_fname = "%s.%s" % (prefix, rfb.r2p.cmd("ps @ 0x%x" % fname_str_addr))
 
             # Get the closest candidate
             fname_addr = sorted([(int(offset)-p, p) for p in rfb.prologues() if p < int(offset)])
@@ -94,10 +107,123 @@ class NamingError(object):
         return real_fname.strip(), faddresses
 
 
+class NamingError(Naming):
+    """
+    Naming strategy based on error strings.
+    """
+
+    @classmethod
+    def drop_string(cls, string):
+        """
+        Discard useless strings.
+        """
+
+        # Regular Expression pattern matching error messages
+        # Examples:
+        #   [APL] (error) %s:%d
+        #   [WPA] (error) <WNK> %s:%d
+        #   [iSD] (err) %s:%d
+        pattern = re.compile(r"\] \(err.?.?\)")
+
+        return re.search(pattern, string) is None
+
+    @classmethod
+    def assemble_instructions(cls, rfb, address):
+        """
+        Assemble MOV instructions that are using the string address.
+
+        Based on observations, they are "MOVU R1, <ADDRESS>" with a 24 bytes
+        immediate aka Major Opcode #13 MOVU.
+        """
+
+        return cls.assemble_mov13(rfb, "R1", address, "\xd1")
+
+    @classmethod
+    def extract_prefix(cls, string):
+        """
+        Extract the function category from the format string.
+        """
+        return string[string.index("[")+1:string.index("]")].replace(" ", "")
+
+    @classmethod
+    def is_valid_pattern(cls, rfb, string, offset):
+        """
+        Check a sequence of instructions to validate the "error" pattern:
+
+        Example:
+            0x00c11796      43d1c5c9       MOVU R1, 0xC9C543           ; r1=0xc9c543 "[APL] (error) %s:%d "
+            0x00c1179a      34d2c5c9       MOVU R2, 0xC9C534           ; r2=0xc9c534 "APPL_endBuffer"
+            0x00c1179e      01c3f901       MOV R3, 505                 ; r3=0x1f9 -> 0xffffff00
+            0x00c117a2      69df1200       BSR fcn.printf
+        """
+
+        # Get the fonction prefix, aka possibe category
+        prefix = cls.extract_prefix(string)
+
+        # Disassemble three instructions and look for the expected sequence
+        instructions = json.loads(rfb.r2p.cmd("pdj 3 @ 0x%x" % offset))
+
+        if not instructions[1]["opcode"].startswith("MOVU R2"):
+            return ""
+
+        # Extract the immediate from the second instruction
+        fname_str_addr = instructions[1]["opcode"].split(",")[1][0:]
+        fname_str_addr = int(fname_str_addr, 16)
+        real_fname = "%s.%s" % (prefix, rfb.r2p.cmd("ps @ 0x%x" % fname_str_addr))
+
+        return real_fname
+
+
+class NamingCamelCase(Naming):
+    """
+    Naming strategy based on CamelCase.
+    """
+
+    @classmethod
+    def drop_string(cls, string):
+        """
+        Discard useless strings.
+        """
+        return not is_camelcase_str(string)
+
+    @classmethod
+    def assemble_instructions(cls, rfb, address):
+        """
+        Assemble MOV instructions that are using the string address.
+
+        Based on observations, they are "MOVU R2, <ADDRESS>" with a 24 bytes
+        immediate aka Major Opcode #13 MOVU.
+        """
+
+        return cls.assemble_mov13(rfb, "R2", address, "\xd2")
+
+    @classmethod
+    def is_valid_pattern(cls, rfb, string, offset):
+        """
+        Check a sequence of instructions to validate the "CamelCase" pattern:
+
+        Example:
+            0x00c3dbe6      e1d184cd       MOVU R1, 0xCD84E1           ; r1=0xcd84e1 "[WPS] (error) %s:%d"
+            0x00c3dbea      84d284cd       MOVU R2, 0xCD8484           ; r2=0xcd8484 "SendEapolFrameToXXXTask"
+            0x00c3dbee      01c37001       MOV R3, 368                 ; r3=0x170 -> 0xffffff00
+            0x00c3dbf2      e9dc4efd       BSR printf
+        """
+
+        instructions = json.loads(rfb.r2p.cmd("pdj 3 @ 0x%x" % offset))
+
+        if not instructions[1]["opcode"].startswith("MOV"):
+            return ""
+        if not instructions[2]["opcode"].startswith("BSR"):
+            return ""
+
+        return string
+
+
 # Naming strategies
 NAMING_STRATEGIES = dict()
 NAMING_STRATEGIES["error"] = NamingError
-#naming_strategies["camelcase"] = naming_camelcase #naming_strategies["printf"] = naming_printf
+NAMING_STRATEGIES["camelcase"] = NamingCamelCase
+#naming_strategies["printf"] = naming_printf
 
 
 def naming_register(parser):
@@ -135,9 +261,13 @@ def naming_command(args):
 
         # Look for an instruction that manipulates this string
         instr_offsets = strategy.lookup(rfb, offset)
+        if not instr_offsets:
+            continue
 
         # Gather candidates
         fname, addresses = strategy.check(rfb, string, instr_offsets)
+        if not fname:
+            continue
         candidates[fname] = candidates.get(fname, set())
         candidates[fname].update(addresses)
 
