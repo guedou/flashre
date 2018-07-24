18db9afdc97105ae78ef34771020f3e2101eabe1
Guillaume Valadon <guillaume@valadon.net>
2018-07-24 17:07:18 +0200

    setup.py and requirements.txt added

---
+++ setup.py
@@ -0,0 +1,12 @@
+# Copyright (C) 2018 Guillaume Valadon <guillaume@valadon.net>
+
+from setuptools import setup
+
+setup(name="flashre",
+      description="Ease Reversing Toshiba FlashAir SD Cards",
+      author="Guillaume Valadon",
+      author_email="guillaume@valadon.net",
+      version="0.1",
+      packages=["flashre"],
+      scripts=["bin/flashre"],
+      )
